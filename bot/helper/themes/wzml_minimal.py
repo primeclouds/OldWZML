0fe8192653f347485d1576287512439fe5d2a0d9
Alpha Network <www.alphanetwork.fun@gmail.com>
2025-01-13 05:05:49 +0000

    Edit wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -3,9 +3,9 @@ class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = '✨Join Channel✨'
-    ST_BN1_URL = 'https://t.me/team_alphanetwork'
+    ST_BN1_URL = 'https://t.me/+tbqV3FdNKAFmYjA9'
     ST_BN2_NAME = '⚡Discussion⚡'
-    ST_BN2_URL = 'https://t.me/Alpha_Violance'
+    ST_BN2_URL = 'https://t.me/+50UM6xPfO282NWFl'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
